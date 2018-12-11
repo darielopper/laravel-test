962fc35ed940d2865fdb373210c737902699cc5a
Dariel <dramos@ecc.cu>
2018-12-11 12:41:11 -0800

    add axios to vue instances

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,8 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+Vue.prototype.$axios = require('axios');
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -18,5 +20,8 @@ window.Vue = require('vue');
 Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    mounted(){
+
+    }
 });
--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -2,6 +2,6 @@
 
 @section("content")
     <div class="container">
-
+        <example></example>
     </div>
 @endsection
