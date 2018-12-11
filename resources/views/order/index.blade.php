34a467e982c322ca62aed5556f70b9ca4007030d
Dariel <dramos@ecc.cu>
2018-12-11 13:06:13 -0800

    creating models and relationships

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,7 +1,37 @@
 @extends("layouts.app")
 
+@section('styles')
+    <style>
+        .label-default{
+            font-size: 18px;
+        }
+    </style>
+@endsection
+
 @section("content")
-    <div class="container">
-        <example></example>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <strong>Orders</strong> <a class="btn btn-success btn-sm pull-right">Add</a>
+                </div>
+                <div class="panel-body">
+                    <table class="table table-condensed table-striped">
+                        <thead>
+                            <th>No.</th>
+                            <th>Offer</th>
+                            <th>Amount</th>
+                            <th>Price</th>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+                <div class="panel-footer">
+                   <strong>TOTAL: </strong> <span class="label label-default">$0</span>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
